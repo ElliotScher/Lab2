ffd59bef09df5a15dce83035c1ca5db5dde99e4b
LoganKippnick <lgkippnick@outlook.com>
2024-01-26 17:34:15 -0500

    inital commit

--- src/main.py
+++ src/main.py
@@ -9,27 +9,51 @@
 
 # Library imports
 from vex import *
-import matplotlib.pyplot
+# import matplotlib.pyplot
 
 # Brain should be defined by default
-brain=Brain()
+brain = Brain()
 
-brain.screen.print("Hello V5")
+leftReflectance = Line(brain.three_wire_port.d)
+rightReflectance = Line(brain.three_wire_port.c)
 
-reflectance = Line(brain.three_wire_port.c)
 button = Bumper(brain.three_wire_port.g)
 
-reflectances = []
-times = []
+leftDrive = Motor(Ports.PORT10, True)
+rightDrive = Motor(Ports.PORT1, False)
+
+WHEEL_DIAMETER_IN = 4
+GEAR_RATIO = 5
+
+FORWARD_SPEED_M_PER_S = 0.1
+LINE_KP = 0.15
 
 while True:
-    reflectances.append(reflectance.reflectivity())
-    times.append(time.time)
-    if (button.pressing()):
-        break
+    while not button.pressing():
+        pass
 
-matplotlib.pyplot.plot(reflectances, times)
-matplotlib.pyplot.xlabel("Time")
-matplotlib.pyplot.ylabel("Reflectance")
-matplotlib.pyplot.title("Reflectance VS Time")
-matplotlib.pyplot.show()
+    while button.pressing():
+        pass
+
+    while True:
+        # dimensional analysis to turn m/s into rpm
+        rpm = (FORWARD_SPEED_M_PER_S * 39.3701 * 60) / (WHEEL_DIAMETER_IN * math.pi)
+
+        # calculated such that error to the right is positive
+        error = leftReflectance.reflectivity() - rightReflectance.reflectivity()
+
+        effort = error * LINE_KP
+
+        leftDrive.spin(FORWARD, (rpm - effort) * GEAR_RATIO)
+        rightDrive.spin(FORWARD, (rpm + effort) * GEAR_RATIO)
+
+        if button.pressing():
+            break
+
+        wait(20)
+
+    leftDrive.stop()
+    rightDrive.stop()
+
+    while button.pressing():
+        pass
