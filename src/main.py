ecf9ee7b7f463c0fc972e8aa5ca0eb0b99c88711
LoganKippnick <lgkippnick@outlook.com>
2024-01-26 17:53:08 -0500

    adding derivative control

--- src/main.py
+++ src/main.py
@@ -27,6 +27,7 @@ GEAR_RATIO = 5
 
 FORWARD_SPEED_M_PER_S = 0.1
 LINE_KP = 0.15
+LINE_KD = 0.15
 
 while True:
     while not button.pressing():
@@ -35,6 +36,7 @@ while True:
     while button.pressing():
         pass
 
+    prevError = 0
     while True:
         # dimensional analysis to turn m/s into rpm
         rpm = (FORWARD_SPEED_M_PER_S * 39.3701 * 60) / (WHEEL_DIAMETER_IN * math.pi)
@@ -42,10 +44,15 @@ while True:
         # calculated such that error to the right is positive
         error = leftReflectance.reflectivity() - rightReflectance.reflectivity()
 
-        effort = error * LINE_KP
+        # change in error per second
+        rate = (error - prevError) / 0.2
+
+        effort = (error * LINE_KP) + (rate * LINE_KD)
 
         leftDrive.spin(FORWARD, (rpm - effort) * GEAR_RATIO)
         rightDrive.spin(FORWARD, (rpm + effort) * GEAR_RATIO)
+
+        prevError = error
 
         if button.pressing():
             break
