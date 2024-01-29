749d586141b337f7e816ea1bd8a05131697f36e1
LoganKippnick <lgkippnick@outlook.com>
2024-01-29 00:11:20 -0500

    adjustments for testing

--- src/main.py
+++ src/main.py
@@ -42,12 +42,16 @@ WHEEL_BASE_IN = 11.625
 GEAR_RATIO = 5
 
 FORWARD_SPEED_M_PER_S = 0.2
-TARGET_DISTANCE_FROM_WALL_IN = 5.5
-WALL_KP_PER_M_PER_S = 10
-WALL_KD_PER_M_PER_S = 1000
+TARGET_DISTANCE_FROM_SIDE_WALL_IN = 5.5
+TARGET_DISTANCE_FROM_FRONT_WALL_IN = 2
+SIDE_WALL_KP_PER_M_PER_S = 25
+SIDE_WALL_KD_PER_M_PER_S = 270
+
+FRONT_WALL_KP = 15
+FRONT_WALL_KD = 5
 
 ROTATION_SPEED_RAD_PER_SEC = math.pi
-TARGET_HEADING = -90
+TARGET_HEADING = 90
 TURN_KP = 4
 TURN_KD = 2.9375
 
@@ -67,7 +71,9 @@ print("IMU CALIBRATED")
 
 def setState(new):
     global state
+    global rotateCounter
     state = new
+
     wait(500)
 
 def idleState():
@@ -75,34 +81,50 @@ def idleState():
     if button.pressing():
         setState(1)
 
+prevFollowError = 0
 def followState():
     global prevError
+    global prevFollowError
     # dimensional analysis to turn m/s into rpm
     rpm = (FORWARD_SPEED_M_PER_S * 39.3701 * 60) / (WHEEL_DIAMETER_IN * math.pi)
 
-    error = sideSonar.distance(DistanceUnits.IN) - TARGET_DISTANCE_FROM_WALL_IN
+    followError = sideSonar.distance(DistanceUnits.IN) - TARGET_DISTANCE_FROM_SIDE_WALL_IN
+
+    wallError = frontSonar.distance(INCHES) - TARGET_DISTANCE_FROM_FRONT_WALL_IN
 
     # change in error per second
-    rate = (error - prevError) / 0.2
+    followRate = (followError - prevFollowError) / 0.2
 
-    effort = (error * WALL_KP_PER_M_PER_S * FORWARD_SPEED_M_PER_S) + (rate * WALL_KD_PER_M_PER_S * FORWARD_SPEED_M_PER_S)
+    followEffort = (followError * SIDE_WALL_KP_PER_M_PER_S * FORWARD_SPEED_M_PER_S) + (followRate * SIDE_WALL_KD_PER_M_PER_S * FORWARD_SPEED_M_PER_S)
 
-    leftCmd = rpm - effort
-    rightCmd = rpm + effort
+    wallRate = (wallError - prevError) / 0.2
 
-    leftDrive.spin(REVERSE, leftCmd * GEAR_RATIO)
-    rightDrive.spin(REVERSE, rightCmd * GEAR_RATIO)
+    wallEffort = (wallError * FRONT_WALL_KP) + (wallRate * FRONT_WALL_KD)
 
-    prevError = error
+    if abs(wallEffort) > rpm:
+            wallEffort = math.copysign(rpm, wallEffort)
 
-    print(sideSonar.distance(DistanceUnits.IN))
 
-    if frontSonar.distance(INCHES) <= 6 and button.pressing():
+    leftDrive.spin(FORWARD, (wallEffort + followEffort) * GEAR_RATIO)
+    rightDrive.spin(FORWARD, (wallEffort - followEffort) * GEAR_RATIO)
+
+    prevError = wallError
+    prevFollowError = followError
+
+    # print(sideSonar.distance(DistanceUnits.IN))
+
+    if button.pressing() or abs(wallError) < 0.05:
+        leftDrive.stop()
+        rightDrive.stop()
+
         prevError = 0
         setState(2)
 
+rotateCounter = 0
 def rotateState():
     global prevError
+    global rotateCounter
+    global TARGET_HEADING
     # dimensional analysis to turn robot rad/sec into wheel rpm
     rpm = (ROTATION_SPEED_RAD_PER_SEC * WHEEL_BASE_IN * 60) / (2 * math.pi)
 
@@ -113,7 +135,7 @@ def rotateState():
     elif error < -180:
         error += 360
 
-    print(imu.heading())
+    # print(imu.heading())
 
     # change in error per second
     rate = (error - prevError) / 0.2
@@ -128,16 +150,28 @@ def rotateState():
 
     prevError = error
 
-    if button.pressing():
+    if button.pressing() or (abs(error) < 0.25 and abs(rate) < 1):
         leftDrive.stop()
         rightDrive.stop()
-        setState(1)
+        rotateCounter += 1
+
+        TARGET_HEADING += 90
+        if(TARGET_HEADING == 360):
+            TARGET_HEADING = 0
+
+        if(rotateCounter >= 4):
+            rotateCounter = 0
+            setState(0)
+        else:
+            setState(1)
 
 while True:
-    print(state)
+    # print(state)
     if state == 0:
         idleState()
     if state == 1:
         followState()
     if state == 2:
         rotateState()
+
+    wait(20)
